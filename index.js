5fd4aea09b156cd16ac53b1fd24ac9c11233c149
AnwarNa <anwar.se19@gmail.com>
2023-11-08 01:49:19 +0800

    add footer and header

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
+
+app.get("/submit", (req, res) => {
+    res.render("index.ejs");
+  });
+  
+app.use(express.static("public"));
+
+app.listen(port,() =>{
+    console.log(`Server is running on port ${port}`);
+});
+
+
 
